2c94429066d58b038d2c2c5b69048579199dd482
crzliang <lkzxgp@gmail.com>
2025-07-17 11:17:51 +0800

    更新链接生成逻辑，使用新的 getPostUrl 函数，添加 permalink 支持

--- src/components/ArchivePanel.svelte
+++ src/components/ArchivePanel.svelte
@@ -3,7 +3,8 @@ import { onMount } from "svelte";
 
 import I18nKey from "../i18n/i18nKey";
 import { i18n } from "../i18n/translation";
-import { getPostUrlBySlug } from "../utils/url-utils";
+import { getPostUrl } from "../utils/url-utils";
+import { type PermalinkData } from "../utils/permalink-utils";
 
 export let tags: string[];
 export let categories: string[];
@@ -21,6 +22,7 @@ interface Post {
 		tags: string[];
 		category?: string;
 		published: Date;
+		permalink?: string;
 	};
 }
 
@@ -105,7 +107,13 @@ onMount(async () => {
 
             {#each group.posts as post}
                 <a
-                        href={getPostUrlBySlug(post.slug)}
+                        href={getPostUrl({
+                            slug: post.slug,
+                            title: post.data.title,
+                            published: post.data.published,
+                            category: post.data.category,
+                            permalink: post.data.permalink
+                        })}
                         aria-label={post.data.title}
                         class="group btn-plain !block h-10 w-full rounded-lg hover:text-[initial]"
                 >
