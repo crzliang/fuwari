21693c7d9f17e2efc7e7a4e9939be6235729b8dc
crzliang <lkzxgp@gmail.com>
2025-09-29 01:54:34 +0800

    更新站点 URL，从 https://blog.crzliang.com/ 更改为 https://blog.crzliang.cn/

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.crzliang.com/",
+	site: "https://blog.crzliang.cn/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
